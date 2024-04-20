7f2972401ef81a1ce17d841661effd9e29768abb
Siim Koppel <siimk999@gmail.com>
2024-04-20 14:29:34 +0300

    Add no argument constructor for flexibility so i can use it with mapToRoomDto method, add validation constraints, use Integer instead of int so i can check validation

--- src/main/java/com/siimkoppel/meetingrooms/dto/RoomDto.java
+++ src/main/java/com/siimkoppel/meetingrooms/dto/RoomDto.java
@@ -1,16 +1,21 @@
 package com.siimkoppel.meetingrooms.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RoomDto {
     private Long id;
+    @NotNull(message = "Please enter room name")
     private String roomName;
-    private int roomNumber;
+    @NotNull(message = "Please enter room number")
+    private Integer roomNumber;
     private boolean booked;
     private LocalDateTime bookedFrom;
     private LocalDateTime bookedTo;
