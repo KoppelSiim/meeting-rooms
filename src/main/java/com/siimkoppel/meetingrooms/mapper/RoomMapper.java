268de15eea1f27acf8a417091303b2061557b1e0
Siim Koppel <siimk999@gmail.com>
2024-04-21 15:05:13 +0300

    Make roomMapper more readable

--- src/main/java/com/siimkoppel/meetingrooms/mapper/RoomMapper.java
+++ src/main/java/com/siimkoppel/meetingrooms/mapper/RoomMapper.java
@@ -6,10 +6,22 @@ import com.siimkoppel.meetingrooms.entity.Room;
 public class RoomMapper {
 
     public static Room mapToRoom(RoomDto roomDto) {
-        return new Room(roomDto.getId(), roomDto.getRoomName(), roomDto.getRoomNumber(), roomDto.isBooked(), roomDto.getBookedFrom(), roomDto.getBookedTo());
+        return new Room(
+                roomDto.getId(),
+                roomDto.getRoomName(),
+                roomDto.getRoomNumber(),
+                roomDto.isBooked(),
+                roomDto.getBookedFrom(),
+                roomDto.getBookedTo());
     }
 
     public static RoomDto mapToRoomDto(Room room) {
-        return new RoomDto(room.getId(), room.getRoomName(), room.getRoomNumber(), room.isBooked(), room.getBookedFrom(), room.getBookedTo());
+        return new RoomDto(
+                room.getId(),
+                room.getRoomName(),
+                room.getRoomNumber(),
+                room.isBooked(),
+                room.getBookedFrom(),
+                room.getBookedTo());
     }
 }
