aec0e74ae9981fa4429bf902d2c23844295a2cb6
Siim Koppel <siimk999@gmail.com>
2024-04-26 12:20:17 +0300

    Use a more modern approach for configuring Freemarker

--- src/main/java/com/siimkoppel/meetingrooms/config/FreemarkerConfig.java
+++ src/main/java/com/siimkoppel/meetingrooms/config/FreemarkerConfig.java
@@ -2,16 +2,20 @@ package com.siimkoppel.meetingrooms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 public class FreemarkerConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.freeMarker();
+    @Bean
+    public FreeMarkerViewResolver freemarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(false);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
     }
 
     @Bean
